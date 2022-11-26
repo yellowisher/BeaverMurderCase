1d821bed266678e1f8aad74260ef34e64958bf5b
yellowisher <auau52@naver.com>
2022-11-27 04:26:28 +0900

    qwewretry

--- Assets/Scripts/GameBook/Pages/PageScriptSimple.cs
+++ Assets/Scripts/GameBook/Pages/PageScriptSimple.cs
@@ -1,15 +1,18 @@
 ﻿using BeaverMurderCase.Dialogue;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace BeaverMurderCase.GameBook.Pages
 {
     public class PageScriptSimple : PageScript
     {
         [SerializeField] private SpeechSet _targetSpeech;
+        [SerializeField] private UnityEvent _afterSpeech;
 
         protected override void OnOpened()
         {
             DialogueManager.Instance.StartSpeechSet(_targetSpeech);
+            _afterSpeech?.Invoke();
         }
     }
 }
