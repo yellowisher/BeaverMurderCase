8f57058713a563ca72042919585a23be88931612
yellowisher <auau52@naver.com>
2022-11-27 03:31:55 +0900

    qewrtyui

---
+++ Assets/Scripts/GameBook/Pages/PageScript_BearHouse.cs
@@ -0,0 +1,38 @@
+﻿using BeaverMurderCase.Dialogue;
+
+namespace BeaverMurderCase.GameBook.Pages
+{
+    public class PageScript_BearHouse : PageScript
+    {
+        protected override void OnOpened()
+        {
+            DialogueManager.Instance.StartSpeechSet("BearHouse");
+        }
+
+        public async void OnClick_Pen()
+        {
+            DialogueManager.Instance.StartSpeechSet("BearHouse_Pen");
+            await DialogueManager.Instance.WaitForSpeechEndAsync();
+            BookManager.Instance.UnlockPage(17);
+        }
+        
+        public async void OnClick_Knife()
+        {
+            DialogueManager.Instance.StartSpeechSet("BearHouse_Knife");
+            await DialogueManager.Instance.WaitForSpeechEndAsync();
+            BookManager.Instance.UnlockPage(15);
+        }
+        
+        public async void OnClick_Refrigerator()
+        {
+            DialogueManager.Instance.StartSpeechSet("BearHouse_Refrigerator");
+            await DialogueManager.Instance.WaitForSpeechEndAsync();
+            BookManager.Instance.UnlockPage(16);
+        }
+
+        public void OnClick_Exit()
+        {
+            DialogueManager.Instance.StartSpeechSet("BearHouse_Exit");
+        }
+    }
+}
